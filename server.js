9967cf458ffd08fe63b8d78ba5481bbee6ad16e0
Nikesh Joshi <nikesh.joshi@office.mypay.com.np>
2026-07-09 10:37:06 +0545

    fix upload rout

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
+// require("dotenv").config();
+
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const path = require("path");
+
+// const app = express();
+
+// app.use(cors({
+//     origin: true,
+//     credentials: true
+// }));
+
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch((err) => {
+//     console.error("MongoDB Error:", err);
+//     process.exit(1);
+//   });
+
+// const authRoutes = require("./routes/auth");
+
+// app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         success: true,
+//         message: "Hridaya Customer Backend is Running 🚀"
+//     });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log("Server running");
+// });
+
 require("dotenv").config();
 
 const express = require("express");
@@ -12,10 +56,11 @@ app.use(cors({
     credentials: true
 }));
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve uploaded images
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -39,5 +84,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log(`Server running on port ${PORT}`);
 });
