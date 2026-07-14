1566c379604c8b2c27156a44eb035c096cf6c8c5
Nikesh Joshi <nikesh.joshi@office.mypay.com.np>
2026-07-14 12:13:15 +0545

    Test register route

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -1,14 +1,27 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
+// });
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT
+  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+
+  family: 4, // Force IPv4
 });
 
 const sendOtpEmail = async (email, otp) => {
